48245b2f0e39873a3eb52d723733d3624d2d9248
yogaraj-stack <yogarajyogaraj178@gmail.com>
2023-09-07 14:40:30 +0530

    test

--- index.php
+++ index.php
@@ -33,6 +33,7 @@ include "libs/load.php";
         exercitationem consequuntur facere tenetur cupiditate, optio nisi, animi ipsam iste delectus aliquam inventore
         ratione. Enim, pariatur!
     </div>
+    <h1>roombu</h1>
     <?php load_temp("footer.php"); ?>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
